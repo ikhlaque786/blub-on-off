a68ff2759923d2cb114605ef5daa26c92824d6e7
ikhlaque786 <ikhlaquea109@gmail.com>
2023-11-19 20:25:43 +0000

    blub on of

---
+++ app.js
@@ -0,0 +1,10 @@
+function bulbon (){
+    document.getElementById("img").src = "https://img.freepik.com/free-vector/realistic-light-bulb-with-electricity_23-2149129410.jpg"
+}
+
+function bulboff (){
+    document.getElementById("img").src = "https://cdn5.vectorstock.com/i/1000x1000/70/44/3d-realistic-off-light-bulb-icon-closeup-vector-27407044.jpg"
+}
+
+
+
